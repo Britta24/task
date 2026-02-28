b6d2846ee6451588a72e5923e7f4aa8b7d10fcda
Lourdhu Britta <yourgithubemail@gmail.com>
2026-02-28 18:47:45 +0530

    fix django settings module path

--- student_community/student_community/wsgi.py
+++ student_community/student_community/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_community.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_community.student_community.settings')
 
 application = get_wsgi_application()
